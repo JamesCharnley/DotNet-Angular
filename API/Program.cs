372b4dff81ad1ab9bdfdc0cc0477a796350de253
JamesCharnley <james.anthony.charnley@gmail.com>
2023-07-26 16:29:15 +1200

    Connect front end to back end with https req

--- API/Program.cs
+++ API/Program.cs
@@ -10,10 +10,13 @@ builder.Services.AddDbContext<DataContext>(opt => //ASPNet
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")); //ASPNet 
 });
+builder.Services.AddCors();
 
 var app = builder.Build(); //ASPNet 
 
 // Configure the HTTP request pipeline.
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+
 app.MapControllers(); //ASPNet 
 
 app.Run(); //ASPNet 
