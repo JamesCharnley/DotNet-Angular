948b17fbfbad0a245cb392ef3106c02c5d76afac
JamesCharnley <james.anthony.charnley@gmail.com>
2023-07-31 12:20:26 +1200

    JWS tokens

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -22,7 +22,7 @@ namespace API.Controllers
         }
 
         [HttpPost("register")]
-        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
 
             if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
@@ -39,12 +39,16 @@ namespace API.Controllers
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
 
-            return user;
+            return new UserDto
+            {
+                Username = user.UserName,
+                Token = _tokenService.CreateToken(user)
+            };
 
         }
 
         [HttpPost("login")]
-        public async Task<ActionResult<AppUser>> Login (LoginDto loginDto)
+        public async Task<ActionResult<UserDto>> Login (LoginDto loginDto)
         {
             var user = await _context.Users.SingleOrDefaultAsync(x =>
                  x.UserName == loginDto.Username);
@@ -60,7 +64,11 @@ namespace API.Controllers
                 if(computedHash[i] != user.PasswordHash[i]) return Unauthorized("invalid password");
             }
 
-            return user;
+            return new UserDto
+            {
+                Username = user.UserName,
+                Token = _tokenService.CreateToken(user)
+            };
         }
 
         private async Task<bool> UserExists(string username)
--- API/Program.cs
+++ API/Program.cs
@@ -1,26 +1,27 @@
+using System.Text;
 using API.Data;
+using API.Extensions;
 using API.Interfaces;
 using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers(); //ASPNet 
-
-builder.Services.AddDbContext<DataContext>(opt => //ASPNet 
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")); //ASPNet 
-});
-
-builder.Services.AddCors();
-builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build(); //ASPNet 
 
 // Configure the HTTP request pipeline.
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers(); //ASPNet 
 
